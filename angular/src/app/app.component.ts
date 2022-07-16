8d5379babdc77c5928505df8116d72f1245f2919
gabgarble <gabgarble@gmail.com>
2022-07-16 13:10:41 +0200

    Not display menu on login view

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
+  public displayMenu: boolean = false;
+
+  constructor(
+    private location: Location
+  ) { }
+
+  ngOnInit() {
+    this.displayMenu = this.location.path() === '/login';
+  }
+
   title = 'Calendar';
 }
--- angular/src/app/shared/components/nav-bar/nav-bar.component.ts
+++ angular/src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MenuItem} from 'primeng/api';
-import { Router,NavigationEnd  } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
