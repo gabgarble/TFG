8cc4d42360d5e2545460b489e6591e87b2bc0b93
gabgarble <gabgarble@gmail.com>
2022-07-18 20:28:18 +0200

    Import calendar view

--- angular/src/app/routes/import-calendar/import-calendar.component.ts
+++ angular/src/app/routes/import-calendar/import-calendar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-import-calendar',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImportCalendarComponent implements OnInit {
 
-  constructor() { }
+  public importCalendarForm: FormGroup;
+
+  public calendarCols: any[] = [
+    {field: "name", header: "Name"},
+    {field: "platform", header: "Platform"},
+    {field: "active", header: "Active"}
+  ];
+
+  public calendars: any[] = [
+    {name: "Calendar 1", platform: "Google", active: true},
+    {name: "Calendar 2", platform: "Google", active: true},
+    {name: "Calendar 3", platform: "Apple", active: false}
+  ];
+
+  public platforms: any[] = [{label: "Google", value: 1},{label: "Apple", value: 2},{label: "Doodle", value: 3}]
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.initImportCalendarForm();
+  }
+
+  public initImportCalendarForm () {
+    this.importCalendarForm = this.formBuilder.group({
+      'name': [null, Validators.required],
+      'platform': [null, Validators.required],
+      'active': [null, Validators.required]
+    });
   }
 
 }
