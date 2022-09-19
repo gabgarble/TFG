4608b553a76030d624dffb704ada0e6e99b8df13
gabgarble <gabgarble@gmail.com>
2022-09-19 07:41:28 +0200

    Settings with user

--- angular/src/app/routes/settings/settings.component.ts
+++ angular/src/app/routes/settings/settings.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/shared/models';
+import { NotificationService } from 'src/app/shared/services/notification.service';
 
 @Component({
   selector: 'app-settings',
@@ -7,12 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
+  public user: User;
   public password: string;
   public repeatPassword: string;
 
-  constructor() { }
+  /* Forms */
+  public userForm: FormGroup;
+
+  constructor(
+      private formBuilder: FormBuilder,
+    private notificationService: NotificationService
+  ) { }
 
   ngOnInit() {
+    this.user = this.notificationService.getAppUser();
+    this.initUserForm();
+  }
+
+  public initUserForm() {
+    this.userForm = this.formBuilder.group({
+      'id': [this.user.id, Validators.nullValidator],
+      'email': [this.user.email, Validators.nullValidator],
+      'name': [this.user.name, Validators.nullValidator],
+      'surname': [this.user.surname, Validators.nullValidator],
+      'username': [this.user.username, Validators.nullValidator],
+      'password': [this.user.password, Validators.nullValidator],
+    });
   }
 
 }
